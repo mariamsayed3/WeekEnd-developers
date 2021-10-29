b665734b68ff8e310dee080b7eefb02c5646353d
Alaa Amr Mansour <alaa.amr1@hotmail.com>
2021-10-29 18:15:42 +0200

    Edit functionality of update & converted css to sass

--- backend/controllers/Admin.js
+++ backend/controllers/Admin.js
@@ -15,8 +15,6 @@ exports.getFlight = async (req, res) => {
 exports.updateFlight = async (req, res) => {
     const flightID = req.params.flightID
     const condition = { id: flightID };
-    const c = { FlightNumber: "4" }
-    console.log(req.body)
     Flight.updateOne(condition, req.body , (error,result)=> {
         if (error) {
           console.log("error", error)
