4478e4803f37724aea032fea2d93a448addb17d9
Alaa Amr Mansour <alaa.amr1@hotmail.com>
2021-10-29 19:14:39 +0200

    Revert "Edit functionality of update & converted css to sass"
    This reverts commit b665734b68ff8e310dee080b7eefb02c5646353d.

--- backend/routes/Admin.js
+++ backend/routes/Admin.js
@@ -5,7 +5,6 @@ const router = new express.Router()
 router.use(express.json())
 
 router.get('/get_flight/:flightID', controllers.getFlight)
-
 router.patch('/update_flight/:flightID', controllers.updateFlight)
 
 module.exports = router
--- frontend/src/Components/Admin/UpdateFlight.js
+++ frontend/src/Components/Admin/UpdateFlight.js
@@ -1,44 +1,32 @@
-import {useState} from 'react'
 import axios from 'axios';
 import 'antd/dist/antd.css';
 import {useParams} from "react-router-dom";
-import {Button, Form, Input, DatePicker, Card, Row, Col, Alert, Modal} from 'antd';
-import '../../Styles/Admin.scss';
-require ('dotenv').config('../../.env')
+import {Button, Form, Input, DatePicker, Card, Row, Col} from 'antd';
+import './Admin.css';
+require ('dotenv').config()
+
 
 
 function UpdateFlight() {
     const id = "617bbcf6edf4585cfd4a5cca"
     const [form] = Form.useForm();
-    const [isModalVisible, setIsModalVisible] = useState(false);
 
     const getFlight = async () =>{
       const {data} = await axios.get (`http://localhost:8000/admin/get_flight/${id}`);
+      console.log(data)
       if(data){
           const {FlightNumber,DepartureAirport,ArrivalAirport, EconomyAvailableSeats,EconomyPrice, BusinessAvailableSeats, BusinessPrice }=data
+
           form.setFieldsValue({FlightNumber, DepartureAirport,ArrivalAirport, EconomyAvailableSeats,EconomyPrice, BusinessAvailableSeats, BusinessPrice  });
         }
       }
       getFlight()
   
-    // const showModal = () => {
-    //     setIsModalVisible(true);
-    //   };
-    //   const handleOk = () => {
-    //     setIsModalVisible(false);
-    //     console.log("Hi")
-    //     Submit();
-    //     console.log("Hi")
-    //   };
-    // const handleCancel = () => {
-    //     setIsModalVisible(false);
-    //   };
 
-    const Submit = async () => {
+    const onSubmit = async () => {
+
         try {
           const values = await form.validateFields();
-          // const values =await form.getFieldsValue(true);
-          console.log(values)
           if (values.DepartureTime) values.DepartureTime = new Date(Date.parse(values.DepartureTime)) 
           if (values.ArrivalTime) values.ArrivalTime = new Date(Date.parse(values.ArrivalTime))
           if (values.EconomyAvailableSeats) values.EconomyAvailableSeats = parseInt(values.EconomyAvailableSeats)
@@ -46,7 +34,6 @@ function UpdateFlight() {
           if (values.BusinessAvailableSeats) values.BusinessAvailableSeats = parseInt(values.BusinessAvailableSeats)
           if (values.BusinessPrice) values.BusinessPrice = parseInt(values.BusinessPrice)
           await axios.patch (`http://localhost:8000/admin/update_flight/${id}`, values);
-         
         } catch (e) {
           console.log(e)
         }
@@ -142,22 +129,12 @@ function UpdateFlight() {
                   </Row>
                 {/* {error && <Alert message={error} type="error" /> } */}
                     <div style={{textAlign:'center'}}>
-                      <Button type="primary" onClick={Submit} style={{ marginTop: '40px', width: '150px'}}>
+                      <Button type="primary" onClick={onSubmit} style={{ marginTop: '40px', width: '150px'}}>
                         Update Flight
                       </Button>
                       </div>
               </Form>
               </div>
-
-              {/* <Modal title="Basic Modal" visible={isModalVisible} onCancel={handleCancel}
-              footer={[
-                <Button key="submit" type="primary" onClick={handleOk}>
-                  Submit
-                </Button>,
-              ]}>
-                <p>Are you sure you want to update this flight?</p>
-                <p> This action cannot be undone!</p>
-              </Modal> */}
           </Card>
         </div>
       )
