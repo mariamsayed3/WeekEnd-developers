902388fa67b785dae32399157558333e180da8bb
Mohamed Abdelhamid <mohamedabdelhamid3030@gmail.com>
2021-10-30 20:38:02 +0200

    navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,14 @@
 import {BrowserRouter as Router, Switch,} from 'react-router-dom'
 import './App.css';
 import UpdateFlight from './Components/Admin/UpdateFlight'
+import Navbar from './Components/General/Navbar';
+import './styles/navbar.scss'
 
 function App() {
   return (
     <Router>
-        <Switch>
-          {/* <PrivateRouteAdmin path='/admin/update_flight/:FlightID' component={UpdateFlight}/>   */}
-          <UpdateFlight />
-        </Switch>
+      <Navbar admin={0}/>
+      <UpdateFlight/>
     </Router>
     
   );
