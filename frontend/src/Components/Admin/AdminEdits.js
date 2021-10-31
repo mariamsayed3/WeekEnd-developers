af335f2a9a9c709633c30ef924d3691591b5060f
mariam wael <mariamwael2000@yahoo.com>
2021-10-31 16:03:25 +0200

    edit flights

---
+++ frontend/src/Components/Admin/AdminEdits.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+import React from "react";
+import axios from "axios";
+import FlightDetails from "./FlightDetails";
+
+export default function AdminEdits() {
+  const [flights, setFlights] = useState([]);
+
+  useEffect(() => {
+    const getFlights = async () => {
+      const { data } = await axios.get(
+        "http://localhost:8000/admin/get_all_flights"
+      );
+      setFlights(data);
+    };
+    getFlights();
+  }, []);
+
+  return (
+    <div style={{ display: "flex", flexDirection: "row", "flex-wrap": "wrap" }}>
+      {flights &&
+        flights.map((flight) => {
+          let id = flight._id;
+          console.log("_id");
+          console.log(id);
+          return <FlightDetails idkey={id} myFlight={flight} />;
+        })}
+    </div>
+  );
+}
