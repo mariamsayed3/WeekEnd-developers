2933d7a9294adb0b687fb84c79a39a186e01ba55
Alaa Amr Mansour <alaa.amr1@hotmail.com>
2021-11-28 12:28:47 +0200

    .

--- frontend/src/Components/General/Clouds.js
+++ frontend/src/Components/General/Clouds.js
@@ -24,7 +24,27 @@ const Clouds = ()=>{
                 <div class="c4">
                 </div>
             </div>
-              
+            <div class="cloud3">
+                <div class="c1">
+                </div>
+                <div class="c2">
+                </div>
+                <div class="c3">
+                </div>
+                <div class="c4">
+                </div>
+                </div>
+
+                <div class="cloud4">
+                <div class="c1">
+                </div>
+                <div class="c2">
+                </div>
+                <div class="c3">
+                </div>
+                <div class="c4">
+                </div>
+                </div>
         </div>
         </div>
 
