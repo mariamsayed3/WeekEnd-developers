f1a8885897fbf849a980b1b56397148ea0cadbbc
maryam-2000 <maryam.ghonim@student.guc.edu.eg>
2021-11-29 14:38:17 +0200

    changed form color

--- frontend/src/Components/User/EditUser.js
+++ frontend/src/Components/User/EditUser.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 import { Form, Input, message, Button, Card } from 'antd';
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import { UserContext } from '../../Context';
 import '../../Styles/EditUser.scss';
+// import $ from 'jquery';
 
 require('dotenv').config()
 
@@ -23,6 +24,40 @@ function EditUser() {
                 .then(() => message.error('Something went wrong.', 3));
         }
     }
+
+    // useEffect(()=>{
+    //     $('.form button').on('click',function(){
+  
+    //         $('.belt').addClass('transitioned-1');
+    //         $('.progress-inner').addClass('loading');
+            
+    //         setTimeout(function(){
+    //           $('.form-container').addClass('transitioned-3');
+    //         },3000);
+            
+    //         setTimeout(function(){
+              
+    //           $('.belt').removeClass('transitioned-1');
+    //           $('.belt').addClass('transitioned-2');
+              
+    //         },3300);
+            
+    //         $('.success button').on('click',function(){
+              
+    //           $('.progress-inner').removeClass('loading');
+    //           $('.belt').removeClass('transitioned-2');
+    //           $('.form-container').removeClass('transitioned-3');
+              
+    //         });
+            
+    //         // setTimeout(function(){
+    //         //   $('.form-container').addClass('transitioned-3');
+    //         // },6000);
+    //         // $('.belt').removeClass('transitioned-2');
+    //         // $('.form-container').removeClass('transitioned-3');
+    //       });
+    // },[]);
+
     return (
         <>
             <div>
